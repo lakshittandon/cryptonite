b51e2bddce125e9bd69334739a0461c3207af29e
LAKSHIT TANDON <143540406+lakshittandon@users.noreply.github.com>
2023-12-28 14:32:36 +0530

    Create bloat.py

---
+++ reverse engineering/bloat.py
@@ -0,0 +1,6 @@
+in this we 2 files and i downloaded them in same directory
+in the hint it was given that the problem was basically of obsufication
+so i guessed the answer is in the main program 
+the main program basically had a lot of refrences to array indexes 
+
+
